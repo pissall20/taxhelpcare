c121a6ef313ce3ea2ab3c167d3ca6b951edb36be
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-07-31 19:35:33 +0530

    Mail backend enabled

--- taxhelpcare/settings.py
+++ taxhelpcare/settings.py
@@ -136,17 +136,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-"""
+
 # SMTP EMAIL BACKEND FOR FUTURE USE
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = #sender's email-id
-EMAIL_HOST_PASSWORD = #password associated with above email-id
-"""
+EMAIL_HOST_USER = 'fastaxhelp@gmail.com'
+EMAIL_HOST_PASSWORD = 'thnyrtctgaccnvxe'
+
 
 PHONENUMBER_DB_FORMAT = "NATIONAL"
 PHONENUMBER_DEFAULT_REGION = "US"
